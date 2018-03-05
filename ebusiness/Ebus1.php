7c0a5881d3c2922925696bc4f40da572ba7b73ea
Laisvunas Vaitiekaitis <117336536@umail.ucc.ie>
2018-03-05 20:48:34 +0000

    Hamburger menu re-adjustment & more

--- ebusiness/Ebus1.php
+++ ebusiness/Ebus1.php
@@ -53,15 +53,10 @@ session_start();
         
     <script>
     function w3_open() {
-      document.getElementById("main").style.marginLeft = "25%";
-      document.getElementById("mySidebar").style.width = "25%";
-      document.getElementById("mySidebar").style.display = "block";
-      document.getElementById("openNav").style.display = 'none';
+        document.getElementById("mySidebar").style.display = "block";
     }
     function w3_close() {
-      document.getElementById("main").style.marginLeft = "0%";
-      document.getElementById("mySidebar").style.display = "none";
-      document.getElementById("openNav").style.display = "inline-block";
+        document.getElementById("mySidebar").style.display = "none";
     }
     </script>
         <div class="content">
